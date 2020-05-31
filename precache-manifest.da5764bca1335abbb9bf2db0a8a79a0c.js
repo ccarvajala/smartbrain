42ee4f3d68dae0e8f5506aa3ef3a6c47b1834414
ccarvajala <carlosjcarvajala@gmail.com>
2020-05-31 16:18:38 -0400

    Updates

---
+++ precache-manifest.da5764bca1335abbb9bf2db0a8a79a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a1a3b64bd1125c88408f1e5dcd467b",
+    "url": "/ccarvajala/smartbrain/index.html"
+  },
+  {
+    "revision": "e3d4c5f17e51c67563e6",
+    "url": "/ccarvajala/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "02461766f13037c9ccb4",
+    "url": "/ccarvajala/smartbrain/static/css/main.4701f083.chunk.css"
+  },
+  {
+    "revision": "e3d4c5f17e51c67563e6",
+    "url": "/ccarvajala/smartbrain/static/js/2.7c45e7f8.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ccarvajala/smartbrain/static/js/2.7c45e7f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02461766f13037c9ccb4",
+    "url": "/ccarvajala/smartbrain/static/js/main.be589172.chunk.js"
+  },
+  {
+    "revision": "081dda5a4521565f43a3",
+    "url": "/ccarvajala/smartbrain/static/js/runtime-main.31f0508c.js"
+  }
+]);
